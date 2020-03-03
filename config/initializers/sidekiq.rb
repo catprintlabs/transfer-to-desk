aaea96de0b6e8523095b7690c5f4007d97a72985
adamcreekroad <adam@adam-george.com>
2020-03-03 15:31:44 -0500

    configure redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,11 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URL'] || "redis://localhost:6379/1" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'] || "redis://localhost:6379/1" }
+end
+
 hash = {
   'Transfer Desk Cases To DB' => {
     'class' => Freshdesk::TransferDeskCasesToDB::Worker,
